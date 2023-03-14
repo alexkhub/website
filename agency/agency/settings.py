e3d1bf2feb3fc1130339f90b9e4195f2c9dcd876
alexkhub <aleksandrkhubaev04@gmail.com>
2023-03-14 20:36:05 +0300

    db

--- agency/agency/settings.py
+++ agency/agency/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oqf1#%iqmp9@@i$4y+qqwcnq4d3e4p+#&*528kp#7*h5hm=m$4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
