d73217c3993b7d91c50219f58afc035897b43e6a
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-03-22 21:14:17 +0300

    admin

--- agency/news/admin.py
+++ agency/news/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(News)
-admin.site.register(Category)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'updated_at') #отображаются в админке
+    list_display_links = ('id', 'title') # перехож при нажатии
+    search_fields = ('title', 'content') # поиск
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- agency/news/models.py
+++ agency/news/models.py
@@ -1,32 +1,39 @@
 from django.db import models
 from django.urls import reverse
 
+
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    ispublic = models.BooleanField(default=True)
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Контен')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования ')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/' , verbose_name='Фотография')
+    ispublic = models.BooleanField(default=True, verbose_name='Публикация')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
-    def __str__ (self):
+    def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk}) #абслоютная ссылка
+        return reverse('post', kwargs={'post_id': self.pk})  # абслоютная ссылка
 
-    class Meta:
-        verbose_name= 'Новость'
+    class Meta:  # админка
+        verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
+        ordering = ['title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    description = models.TextField()
+    name = models.CharField(max_length=100, db_index=True , verbose_name="Название")
+    description = models.TextField( verbose_name="Описание")
 
-
-    def __str__ (self):
+    def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'category_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-name'] # обратное направление
