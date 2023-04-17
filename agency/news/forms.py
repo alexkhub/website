25c24e88dde25c759a5a4c3f90c2c91c0c246118
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-04-17 16:39:48 +0300

    form

--- agency/news/forms.py
+++ agency/news/forms.py
@@ -4,12 +4,12 @@ from .models import *
 
 # формы
 class Add_Real_estateForm(forms.Form):
-    user = forms.ModelChoiceField(queryset=User.objects.all())
-    city = forms.ModelChoiceField(queryset=City.objects.all())
-    street = forms.CharField(max_length=50, )
-    address = forms.CharField(max_length=50, )
-    category_name = forms.ModelChoiceField(queryset=Сategories_real_estate.objects.all())
-    services = forms.ModelChoiceField(queryset=Services.objects.all())
-    aray = forms.FloatField()
-    price = forms.FloatField()
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'row': 10}))
+    user = forms.ModelChoiceField(queryset=User.objects.all(), label='Никнейм')
+    city = forms.ModelChoiceField(queryset=City.objects.all(), label='Город', empty_label='Город не выбран')
+    street = forms.CharField(max_length=50, label='Улица')
+    address = forms.CharField(max_length=50, label='Адрес')
+    category_name = forms.ModelChoiceField(queryset=Сategories_real_estate.objects.all(), label='Категория', empty_label='Категория не выбрана')
+    services = forms.ModelChoiceField(queryset=Services.objects.all(), label='Продать / Сдать в аренду', empty_label='Услуга не выбрана')
+    aray = forms.FloatField(label='Площадь')
+    price = forms.FloatField(label='Цена')
+    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'row': 10}) , label='Описание', required=False)
