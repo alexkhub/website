e95524b81abfac1bfed986d0d059e5cf470bff96
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-05-17 17:36:29 +0300

    pog

--- agency/news/urls.py
+++ agency/news/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('enter/', enter, name='enter'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('add_real_estate/', Add_Real_Esate.as_view(), name='add_real_estate'),
-    path('show_real_estate/<slug:real_estate_slug/', Show_Real_Estate.as_view(), name='show_real_estate'),
+    path('show_real_estate/<slug:real_estate_slug>/', Show_Real_Estate.as_view(), name='show_real_estate'),
 
 ]
